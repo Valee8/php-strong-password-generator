9441b60785b27f93108b46f64a032278e812aba7
Vale <oirelav95@gmail.com>
2023-01-11 23:52:47 +0100

    aggiungo funzione per generare password

--- index.php
+++ index.php
@@ -25,5 +25,28 @@
 
         <input type="submit" value="Invia">
 
+        <br>
+
+    <?php
+
+        $lengthPassword = $_GET['lengthPassword'] ?? 0;
+
+        function generatePassword($lengthPassword) {
+            $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?$%^&*()_-+=[]{}:,;@|<>./";
+
+            $lengthCharacters = strlen($characters);
+
+            $password = "";
+
+            for ($i = 0; $i < $lengthPassword; $i++) {
+                $password .= $characters[rand(0, $lengthCharacters - 1)];
+            }
+
+            return $password;
+        }
+
+        echo generatePassword($lengthPassword);
+    ?>
+
 </body>
 </html>
