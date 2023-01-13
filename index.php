4c39ea923d8972c54f276c30bb0fb3f5fba4dde4
Vale <oirelav95@gmail.com>
2023-01-13 01:39:39 +0100

    aggiungo opzioni per generazione password

--- index.php
+++ index.php
@@ -12,41 +12,82 @@
         session_start();
 
         require_once __DIR__ . '/partials/helper.php';
+
+        $lengthPassword = $_GET['lengthPassword'] ?? 0;
+        $filterNumbers = $_GET['numbers'] ?? false;
+        $filterUppercaseLetters = $_GET['uppercaseLetters'] ?? false;
+        $filterLowercaseLetters = $_GET['lowercaseLetters'] ?? false;
+        $filterSymbols = $_GET['symbols'] ?? false;
+
+        if ($lengthPassword && ($filterNumbers || $filterUppercaseLetters || $filterLowercaseLetters || $filterSymbols)) {
+
+            $passw = $_SESSION['password'];
+            
+            $_SESSION['password'] = generatePassword($lengthPassword, $filterNumbers, $filterUppercaseLetters, $filterLowercaseLetters, $filterSymbols);
+
+            header('Location: password.php');
+        }
     ?>
 
 </head>
 <body>
 
-    <div class="container text-center">
-        <h1>
+    <div class="container text-left w-50 bg-primary my-5 rounded p-3">
+        <h1 class="text-center">
             Strong Password Generator
         </h1>
 
-        <h2>
+        <h2 class="text-center">
             Genera una password sicura
         </h2>
 
-        <form>
-            <label for="lengthPassword" class="col-form-label">Lunghezza password: </label>
-
-            <input type="text" name="lengthPassword">
-
-            <input type="submit" value="Invia" class="btn btn-primary">
-
-        </form>    
-
-        <br>
-
         <?php
 
-            if ($lengthPassword) {
-                
-                $_SESSION['password'] = generatePassword($lengthPassword);
-
-                header('Location: ./password.php');
+            if ($lengthPassword && (!$filterNumbers && !$filterUppercaseLetters && !$filterLowercaseLetters && !$filterSymbols)) {
+                echo "<h4 class='text-center text-danger'>Scegli almeno una delle opzioni</h4>";
+            }
+            if ($lengthPassword === "") {
+                echo "<h4 class='text-center text-danger'>Inserisci lunghezza password</h4>";
             }
 
         ?>
+
+        <form class="bg-white text-secondary rounded p-2">
+            <div class="w-50 mx-auto">
+                <div class="text-center">
+                    <label for="lengthPassword" class="col-form-label pe-2">Lunghezza password: </label>
+
+                    <input type="text" name="lengthPassword">
+                </div>
+
+                <input type="checkbox" name="numbers">
+
+                <label for="numbers">Numbers </label>
+
+                <br>
+
+                <input type="checkbox" name="uppercaseLetters">
+
+                <label for="uppercaseLetters">Maiuscole </label>
+
+                <br>
+
+                <input type="checkbox" name="lowercaseLetters">
+
+                <label for="lowercaseLetters">Minuscole </label>
+
+                <br>
+
+                <input type="checkbox" name="symbols">
+
+                <label for="symbols">Simboli </label>
+
+                <br>
+
+                <input type="submit" value="Invia" class="btn btn-danger mt-2">
+            </div>
+        </form>    
+            
     </div>
 
 </body>
